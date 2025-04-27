af2c203650e9cf8c5f6d2a3ba5b1c9a34d0cb50f
jelsey <james.elsey@gmail.com>
2025-04-27 20:43:32 +1000

    stuff

--- src/drive_controller/launch/drive_controller.launch.py
+++ src/drive_controller/launch/drive_controller.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='drive_controller',
-            executable='drive_controller',
+            executable='drive_controller',  # matches setup.py 'console_scripts'
             name='drive_controller',
             output='screen'
         )
--- src/drive_controller/setup.py
+++ src/drive_controller/setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'drive_controller'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/' + package_name + '/launch', ['launch/drive_controller.launch.py']),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
